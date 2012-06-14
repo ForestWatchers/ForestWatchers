5058e06a0ee08a5f5e48014c9903185d95690e6e
Eduardo Luz <eduardofpl@gmail.com>
2012-06-14 14:52:00 -0300

    Adding copyright for INPE at the addLayers script. Closes #10

--- contrib/addLayer.py
+++ contrib/addLayer.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
+# Copyright (C) 2012 Instituto Nacional de Pesquisas Espaciais (INPE)
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # 
 # This script compares files from a directory to those contemplated
 # in a specific mapfile. If the file is not in the mapfile it creates
